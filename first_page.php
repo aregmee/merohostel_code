5a8c040181e59da3ff24d98d0718395ef9d55f2a
Ashim Regmi <ashim.regmi@deerwalk.edu.np>
2015-06-13 21:27:38 +0545

    Footer seperated in a php file

--- first_page.php
+++ first_page.php
@@ -363,56 +363,8 @@
 			</div>
 			<!-- content -->
 
-			<div id="footer">
-
-				<div id="mainFooter">
-					<div class="container">
-						<div class="row">
-							<div class="col-sm-3">
-								<ul>
-									<li>
-										Company
-									</li>
-									<li>
-										Agreement
-									</li>
-									<li>
-										Privacy
-									</li>
-								</ul>
-							</div>
-							<div class="col-sm-4">
-								<ul>
-									<li>
-										<i class="fa fa-facebook-square"></i>
-										Facebook
-									</li>
-									<li>
-										<i class="fa fa-twitter-square"></i>
-										Twitter
-									</li>
-									<li>
-										<i class="fa fa-google-plus-square"></i>
-										Google Plus
-									</li>
-								</ul>
-							</div>
-							<div class="col-sm-5">
-								<ul>
-									<li>
-										Hosting Partner subisu
-									</li>
-									<li>
-										Copyright © 2015, Sodhpuch Pvt. Ltd.
-									</li>
-								</ul>
-							</div>
-
-						</div><!--row -->
-					</div><!--container -->
-				</div><!-- mainFooter -->
-
-			</div><!-- footer -->
+            <?php include "footer.php";?>
+			<!-- footer -->
 		</div>
 
 		<!-- Main Script -->
--- second_page.php
+++ second_page.php
@@ -266,54 +266,7 @@
 			</div>
 			<!-- content -->
 
-			<div id="footer">
-
-				<div id="mainFooter">
-					<div class="container">
-						<div class="row">
-							<div class="col-sm-3">
-								<ul>
-									<li>
-										Company
-									</li>
-									<li>
-										Agreement
-									</li>
-									<li>
-										Privacy
-									</li>
-								</ul>
-							</div>
-							<div class="col-sm-4">
-								<ul>
-									<li>
-										<i class="fa fa-facebook-square"></i>
-										Facebook
-									</li>
-									<li>
-										<i class="fa fa-twitter-square"></i>
-										Twitter
-									</li>
-									<li>
-										<i class="fa fa-google-plus-square"></i>
-										Google Plus
-									</li>
-								</ul>
-							</div>
-							<div class="col-sm-5">
-								<ul>
-									<li>
-										Hosting Partner subisu
-									</li>
-									<li>
-										Copyright © 2015, Sodhpuch Pvt. Ltd.
-									</li>
-								</ul>
-							</div>
-
-						</div><!--row -->
-					</div><!--container -->
-				</div><!-- mainFooter -->
+            <?php include "footer.php";?>
 
 			</div><!-- footer -->
 		</div>
