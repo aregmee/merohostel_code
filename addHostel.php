792e16dd0d20467bd13a425bd7da9e6f618aa2c2
asimregmi <ashim.regmi@deerwalk.edu.np>
2015-07-09 09:03:27 +0545

    add hostel feature added

--- addHostel.php
+++ addHostel.php
@@ -1,4 +1,51 @@
 <!DOCTYPE HTML>
+<?php
+include("DBConnection.php");
+$name = $_POST["hname"];
+$gender = $_POST["sex"];
+$address = $_POST["address"];
+$honame = $_POST["honame"];
+$cno = $_POST["cno"];
+$edate = $_POST["edate"];
+$capacity = $_POST["tno"];
+$adfee = $_POST["admissionfee"];
+$sdepo = $_POST["sdepo"];
+$mf1 = $_POST["mf1"];
+$mf2 = $_POST["mf2"];
+$mf3 = $_POST["mf3"];
+$mf4 = $_POST["mf4"];
+
+if(isset($name) && isset($gender) && isset($address)){
+
+    $sql = "INSERT INTO temp_fee_structure (admission, security_deposit, 1_bed, 2_bed, 3_bed, 4_bed)
+	VALUES('$adfee','$sdepo','$mf1','$mf2','$mf3','$mf4')";
+    mysqli_query($conn, $sql);
+
+    $last_id = "SELECT id FROM temp_fee_structure ORDER BY id desc";
+    $ids = $conn->query($last_id);
+    $ids_row = $ids->fetch_assoc();
+    $fee_id = $ids_row["id"];
+
+    $sql = "INSERT INTO temp_hostel (name ,gender ,address ,estd_year,fee_structure_id,capacity)
+	VALUES ('$name ','	$gender ','$address ','$edate ','$fee_id','$tno ')";
+    mysqli_query($conn, $sql);
+
+    $last_id = "SELECT id FROM temp_hostel ORDER BY id desc";
+    $ids = $conn->query($last_id);
+    $ids_row = $ids->fetch_assoc();
+    $hos_id = $ids_row["id"];
+    $sql = "INSERT INTO temp_owner (hostel_id ,name ,contact)
+	VALUES ('$hos_id ',' $honame ','$cno ')";
+    mysqli_query($conn, $sql);
+
+
+    foreach($_POST['facility'] as $selected){
+        $sql = "INSERT INTO temp_hostel_facility (hostel_id, facility_id) VALUES('$hos_id ', '$selected')";
+        mysqli_query($conn, $sql);
+    }
+    $submitted = "true";
+}
+?>
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -73,14 +120,22 @@
 						<div class="col-md-8">
 							<h2 id="deHosTtl">Add Hostel</h2>
 							<div class="row" style="margin-top: -15px;">
-
-								<form id="addHostel" class="ui form" style="width: 97%;"  method="post" >
+                                <?php if(isset($submitted)){
+                                    if ($submitted == "true"){?>
+                                        <div class="ui positive message">
+                                            <div class="header">
+                                                Thank you for adding the hostel. We will verify the information before updating on the website.
+                                            </div>
+                                        </div>
+                                    <?php }
+                                } ?>
+								<form id="addHostel" class="ui form" style="width: 97%;"  method="post" action="addHostel.php" enctype="multipart/form-data">
 
 									<h3 id="facilities"><a name="facilities" id="facilities"></a>General Information</h3>
 									<div class="field">
 										<label>Hostel Name</label>
 										<div class="ui corner labeled input">
-											<input placeholder="Hostel Name" type="text"  name=""  required="true">
+											<input placeholder="Hostel Name" type="text"  name="hname"  required="true">
 											<div class="ui corner label">
 												<i class="asterisk icon"></i>
 											</div>
@@ -91,14 +146,14 @@
 										<div class="inline fields">											
 											<div class="field">
 												<div class="ui radio checkbox">
-													<input type="radio" name="gender" checked="checked">
-													<label>Male</label>
+													<input type="radio" name="sex" checked="checked" value="boys">
+													<label>Boys</label>
 												</div>
 											</div>
 											<div class="field">
 												<div class="ui radio checkbox">
-													<input type="radio" name="gender">
-													<label>Female</label>
+													<input type="radio" name="sex" value="girls">
+													<label>Girls</label>
 												</div>
 											</div>
 										</div>
@@ -106,7 +161,7 @@
 									<div class="field">
 										<label>Address</label>
 										<div class="ui corner labeled input">
-											<input placeholder="Address" type="text"  name=""  required="true">
+											<input placeholder="Address" type="text"  name="address"  required="true">
 											<div class="ui corner label">
 												<i class="asterisk icon"></i>
 											</div>
@@ -115,9 +170,8 @@
 									<div class="field">
 										<label>Hostel Owner's Name</label>
 										<div class="ui corner labeled input">
-											<input placeholder="Hostel Owner's Name" type="text"  name=""  required="true">
+											<input placeholder="Hostel Owner's Name" type="text"  name="honame" >
 											<div class="ui corner label">
-												<i class="asterisk icon"></i>
 											</div>
 										</div>
 									</div>
@@ -125,18 +179,17 @@
 										<div class="field">
 											<label>Contact Number</label>
 											<div class="ui corner labeled input">
-												<input placeholder="Contact Number" type="number"  name=""  required="true">
+												<input placeholder="Contact Number" type="text"  name="cno"  required="true">
 												<div class="ui corner label">
-													<i class="asterisk icon"></i>
+                                                    <i class="asterisk icon"></i>
 												</div>
 											</div>
 										</div>
 										<div class="field">
 											<label>Established Year</label>
 											<div class="ui corner labeled input">
-												<input placeholder="Established Year" type="number"  name=""  required="true">
+												<input placeholder="Established Year" type="text"  name="edate"  >
 												<div class="ui corner label">
-													<i class="asterisk icon"></i>
 												</div>
 											</div>
 										</div>
@@ -147,50 +200,44 @@
 									<div class=" fields">
 										<div class="field">
 											<div class="ui checkbox">
-												<input type="checkbox" name="facility" value="">
+												<input type="checkbox" name="facility[]" value="1">
 												<label>24 electricity</label>
 											</div>
 										</div>
 										<div class="field">
 											<div class="ui  checkbox">
-												<input type="checkbox" name="facility" value="">
+												<input type="checkbox" name="facility[]" value="2">
 												<label>Wi-Fi</label>
 											</div>
 										</div>
 										<div class="field">
 											<div class="ui  checkbox">
-												<input type="checkbox" name="facility" value="">
+												<input type="checkbox" name="facility[]" value="3">
 												<label>Hot Water</label>
 											</div>
 										</div>
 										<div class="field">
 											<div class="ui  checkbox">
-												<input type="checkbox" name="facility" value="">
+												<input type="checkbox" name="facility[]" value="4">
 												<label>Locker</label>
 											</div>
 										</div>
 										<div class="field">
 											<div class="ui  checkbox">
-												<input type="checkbox" name="facility" value="">
+												<input type="checkbox" name="facility[]" value="5">
 												<label>Hostel Warden</label>
 											</div>
 										</div>
 										<div class="field">
 											<div class="ui  checkbox">
-												<input type="checkbox" name="facility" value="">
+												<input type="checkbox" name="facility[]" value="6">
 												<label>Laundry</label>
 											</div>
 										</div>
 										<div class="field">
 											<div class="ui  checkbox">
-												<input type="checkbox" name="facility" value="">
+												<input type="checkbox" name="facility[]" value="7">
 												<label>Telivision</label>
-											</div>
-										</div>
-										<div class="field">
-											<div class="ui  checkbox">
-												<input type="checkbox" name="facility" value="">
-												<label>Gym</label>
 											</div>
 										</div>
 									</div>
@@ -199,9 +246,8 @@
 										<div class="field">
 											<label>Capacity</label>
 											<div class="ui corner labeled input">
-												<input placeholder="Capacity" type="number" id = "telephone" name="telephone" maxlength="30" size="30" required="true">
+												<input placeholder="Capacity" type="number" id = "capacity" name="tno" maxlength="30" size="30">
 												<div class="ui corner label">
-													<i class="asterisk icon"></i>
 												</div>
 											</div>
 										</div>
@@ -213,18 +259,16 @@
 										<div class="field">
 											<label>Admission</label>
 											<div class="ui corner labeled input">
-												<input placeholder="Admission" type="number" id = "telephone" name="telephone" maxlength="30" size="30" required="true">
+												<input placeholder="Admission" type="number" id = "admission_fee" name="admissionfee" maxlength="30" size="30">
 												<div class="ui corner label">
-													<i class="asterisk icon"></i>
 												</div>
 											</div>
 										</div>
 										<div class="field">
 											<label>Security Deposit</label>
 											<div class="ui corner labeled input">
-												<input placeholder="Security Deposit" type="number" id = "telephone" name="telephone" maxlength="30" size="30" required="true">
+												<input placeholder="Security Deposit" type="number" id = "sec_dep" name="sdepo" maxlength="30" size="30">
 												<div class="ui corner label">
-													<i class="asterisk icon"></i>
 												</div>
 											</div>
 										</div>
@@ -233,36 +277,32 @@
 										<div class="field">
 											<label>1 Bed</label>
 											<div class="ui corner labeled input">
-												<input placeholder="1 Bed" type="number" id="first_name" name="first_name" maxlength="50" size="30" required="true">
+												<input placeholder="1 Bed" type="number" id="one_bed" name="mf1" maxlength="50" size="30">
 												<div class="ui corner label">
-													<i class="asterisk icon"></i>
 												</div>
 											</div>
 										</div>
 										<div class="field">
 											<label>2 Bed</label>
 											<div class="ui corner labeled input">
-												<input placeholder="2 Bed" type="number" id ="last_name" name="last_name" maxlength="50" size="30" required="true">
+												<input placeholder="2 Bed" type="number" id ="two_bed" name="mf2" maxlength="50" size="30">
 												<div class="ui corner label">
-													<i class="asterisk icon"></i>
 												</div>
 											</div>
 										</div>
 										<div class="field">
 											<label>3 Bed</label>
 											<div class="ui corner labeled input">
-												<input placeholder="3 Bed" type="number" id="first_name" name="first_name" maxlength="50" size="30" required="true">
+												<input placeholder="3 Bed" type="number" id="three_bed" name="mf3" maxlength="50" size="30">
 												<div class="ui corner label">
-													<i class="asterisk icon"></i>
 												</div>
 											</div>
 										</div>
 										<div class="field">
 											<label>4 Bed</label>
 											<div class="ui corner labeled input">
-												<input placeholder="4 Bed" type="number" id ="last_name" name="last_name" maxlength="50" size="30" required="true">
+												<input placeholder="4 Bed" type="number" id ="four_bed" name="mf4" maxlength="50" size="30">
 												<div class="ui corner label">
-													<i class="asterisk icon"></i>
 												</div>
 											</div>
 										</div>
